291722680e0b8c54cf8eb6b65f10090db3ec648d
Jonathan Pelletier <jopela@hedgenet.ca>
2018-07-20 09:09:41 -0400

    Reset base project files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Hedgenet engineering',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -10,19 +10,12 @@ const Layout = ({ children, data }) => (
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'Hedgenet Engineering' },
+        { name: 'keywords', content: 'Hedgenet, Engineering' },
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div>
       {children()}
     </div>
   </div>
